9fbfee106bb06f4f248827958e200356960ce93d
ycccccccy <asdfghjkl2446@foxmail.com>
2025-12-29 20:57:32 +0800

    update files

--- test/widgets/app_smoke_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:echotrace/main.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  testWidgets('EchoTrace app smoke test', (tester) async {
-    await tester.pumpWidget(const EchoTraceApp());
-    expect(find.text('EchoTrace'), findsWidgets);
-  });
-}
